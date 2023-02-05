b157cde57a0f4ac20d3c6736451d21001aa8b119
abhinav1925 <31373821+abhinav1925@users.noreply.github.com>
2023-02-05 15:38:03 -0500

    add player pos tracking

--- Assets/Scripts/handAttack.cs
+++ Assets/Scripts/handAttack.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class handAttack : MonoBehaviour
@@ -7,7 +8,10 @@ public class handAttack : MonoBehaviour
     [SerializeField] private bool handAttackCheck = false;
     [SerializeField] private Animator anim;
     [SerializeField] private Vector2 playerPosition;
-
+    public float speed = 1.0F;
+    public Transform startMarker;
+    private float startTime;
+    private float journeyLength;
     private GameObject playerObj = null;
 
     // Start is called before the first frame update
@@ -25,12 +29,21 @@ public class handAttack : MonoBehaviour
     {
         if(handAttackCheck == true){
             anim.SetTrigger("Scratch");
+            HandPosUpdate();
         }
+
+
     }
     void AttackPlayer()
     {
         // Go to player position and scratch
+        
         playerPosition = playerObj.transform.position;
+        startTime = Time.time;
+
+        // Calculate the journey length.
+        journeyLength = Vector3.Distance(startMarker.position, playerPosition);
+
     }
     IEnumerator RandomAttackTimer()
     {
@@ -39,5 +52,20 @@ public class handAttack : MonoBehaviour
             AttackPlayer();
             yield return new WaitForSeconds(Random.Range(1, 5));
         }
+    }
+
+    private void HandPosUpdate ()
+    {
+        
+
+        // Distance moved equals elapsed time times speed..
+        float distCovered = (Time.time - startTime) * speed;
+
+        // Fraction of journey completed equals current distance divided by total distance.
+        float fractionOfJourney = distCovered / journeyLength;
+
+        // Set our position as a fraction of the distance between the markers.
+        transform.position = Vector3.Lerp(startMarker.position, playerPosition, fractionOfJourney);
+
     }
 }
